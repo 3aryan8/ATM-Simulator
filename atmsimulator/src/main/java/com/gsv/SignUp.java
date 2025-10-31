c075376459e2c14a7d9ebb0844baf23048a47a03
3aryan8 <3aryan8@gmail.com>
2025-10-31 18:47:25 +0530

    added SignUp

---
+++ atmsimulator/src/main/java/com/gsv/SignUp.java
@@ -0,0 +1,24 @@
+package com.gsv;
+
+import javax.swing.*;
+
+public class SignUp  extends JFrame{
+
+
+    JLabel L_signup, L_user, L_name ,
+    SignUp(){
+
+        super("SIGNUP");
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+
+
+
+        setBounds();
+        setVisible(true);
+
+
+    }
+}
